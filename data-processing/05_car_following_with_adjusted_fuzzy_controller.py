3868bb13201e2ba13338f1d93a9b51e7b11c69dd
blazaid <alberto.da@gmail.com>
2018-03-20 17:12:50 +0100

    Script para entrenar el controlador difuso. Como no termina de descomprimirse los datos en el grande lo subo para pillarlo en el pequeño y hacer los primeros tests

--- data-processing/05_car_following_with_adjusted_fuzzy_controller.py
+++ data-processing/05_car_following_with_adjusted_fuzzy_controller.py
@@ -1,88 +1,125 @@
 import os
 import shutil
-from datetime import datetime
+from multiprocessing import Process
 
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 from tfz import IVar, OVar, fuzzy_controller
 
-SUBJECT = 'miguel'
+SUBJECT = 'edgar'
 DATASETS_PATH = '/media/blazaid/Saca/Phd/data/datasets'
-MOMENTS = 't-t5-t10-t20'
 LEARNING_RATE = 0.01
-TRAIN_STEPS = 10000
-LOGS_STEPS = 100
+TRAIN_STEPS = 1000
+LOGS_STEPS = 10
+NUM_FS = [3, 4, 2, 2, 2, 4, 5]
 
-tensorboard_path = 'tmp'
+input_cols = (
+    'Leader distance', 'Next TLS distance', 'Next TLS green', 'Next TLS yellow',
+    'Next TLS red', 'Speed', 'Speed to leader')
+output_col = 'Acceleration'
+
+train_file = os.path.join(DATASETS_PATH, 'cf-{}-training.csv'.format(SUBJECT))
+test_file = os.path.join(DATASETS_PATH, 'cf-{}-validation.csv'.format(SUBJECT))
+
+num_fs_string = '-'.join(str(x) for x in NUM_FS)
+
+summary_trn_path = 'tensorboard/{}/{}/training'.format(SUBJECT, num_fs_string)
+summary_val_path = 'tensorboard/{}/{}/validation'.format(SUBJECT, num_fs_string)
+summary_tst_path = 'tensorboard/{}/{}/test'.format(SUBJECT, num_fs_string)
+
+if os.path.exists(summary_trn_path):
+    shutil.rmtree(summary_trn_path)
+if os.path.exists(summary_val_path):
+    shutil.rmtree(summary_val_path)
+if os.path.exists(summary_tst_path):
+    shutil.rmtree(summary_tst_path)
+
+
+def launch_tensorboard(tb_trn_path, tb_val_path, tb_tst_path):
+    os.system('tensorboard --logdir=training:{},validation:{},test:{}'.format(tb_trn_path, tb_val_path, tb_tst_path))
+
+    print('Tensorboard started on http://localhost:6006/'.format())
+
 
 if __name__ == '__main__':
-    train_file = os.path.join(DATASETS_PATH, 'cf-{}-training-t-t5-t10-t20.csv'.format(SUBJECT))
-    test_file = os.path.join(DATASETS_PATH, 'cf-{}-validation-t-t5-t10-t20.csv'.format(SUBJECT))
+    tb_process = Process(target=launch_tensorboard, args=(summary_trn_path, summary_val_path))
+
+    # Fuzzy controller graph
+    x, y_hat = fuzzy_controller(
+        i_vars=[
+            IVar(name='LeaderDistance', fuzzy_sets=NUM_FS[0], domain=(0., 1.)),
+            IVar(name='NextTlsDistance', fuzzy_sets=NUM_FS[1], domain=(0., 1.)),
+            IVar(name='NextTlsGreen', fuzzy_sets=NUM_FS[2], domain=(0., 1.)),
+            IVar(name='NextTlsYellow', fuzzy_sets=NUM_FS[3], domain=(0., 1.)),
+            IVar(name='NextTlsRed', fuzzy_sets=NUM_FS[4], domain=(0., 1.)),
+            IVar(name='Speed', fuzzy_sets=NUM_FS[5], domain=(0., 20.)),
+            IVar(name='SpeedToLeader', fuzzy_sets=NUM_FS[6], domain=(-20., 20.)),
+        ],
+        o_var=OVar(name='Acceleration', values=(-1, 1))
+    )
+
+    # Expected output
+    y = tf.placeholder(tf.float32)
+
+    # Training graph
+    cost = tf.reduce_mean(tf.squared_difference(y, y_hat))
+    train = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cost)
+
+    tf.summary.scalar('cost', cost)
+    merged_summary = tf.summary.merge_all()
+    writer_trn = tf.summary.FileWriter(summary_trn_path)
+    writer_val = tf.summary.FileWriter(summary_val_path)
+    writer_tst = tf.summary.FileWriter(summary_tst_path)
+
     train_df = pd.read_csv(train_file, index_col=False).astype(np.float32)
     test_df = pd.read_csv(test_file, index_col=False).astype(np.float32)
 
-    tf.reset_default_graph()
-
-    # Input variables
-    leader_distance = IVar(name='LeaderDist', fuzzy_sets=3, domain=(0., 1.))
-    next_tls_distance = IVar(name='NextTlsDist', fuzzy_sets=4, domain=(0., 1.))
-    next_tls_status = IVar(name='NextTlsStatus', fuzzy_sets=3, domain=(0., 1.))
-    relative_speed = IVar(name='RelativeSpeed', fuzzy_sets=3, domain=(0., 2.))
-    speed_to_leader = IVar(name='SpeedToLeader', fuzzy_sets=3, domain=(-1., 1.))
-
-    # Output variable
-    acceleration = OVar(name='Acceleration', values=(-1, 1))
-
-    # Controller
-    x, y_hat = fuzzy_controller(
-        i_vars=[leader_distance, next_tls_distance, next_tls_status, relative_speed, speed_to_leader],
-        o_var=acceleration
-    )
-
-    # Training process
-    y = tf.placeholder(tf.float32)
-    with tf.name_scope('train'):
-        cost = tf.reduce_mean(tf.squared_difference(y, y_hat))
-        train = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cost)
-        tf.summary.scalar('cost', cost)
-
-    merged_summary = tf.summary.merge_all()
-    if os.path.exists(tensorboard_path + '/training'):
-        shutil.rmtree(tensorboard_path + '/training')
-    if os.path.exists(tensorboard_path + '/test'):
-        shutil.rmtree(tensorboard_path + '/test')
-
-    writer_trn = tf.summary.FileWriter(tensorboard_path + '/training')
-    writer_tst = tf.summary.FileWriter(tensorboard_path + '/test')
-
-    inputs = train_df[
-        ['Leader distance', 'Next TLS distance', 'Next TLS status', 'Relative speed', 'Speed to leader']
-    ].values
-    output = train_df[['Acceleration']].values
-    inputs_test = test_df[
-        ['Leader distance', 'Next TLS distance', 'Next TLS status', 'Relative speed', 'Speed to leader']
-    ].values
-    output_test = test_df[['Acceleration']].values
-
+    tb_process.start()
     with tf.Session() as session:
         session.run(tf.global_variables_initializer())
-
         writer_trn.add_graph(session.graph)
 
-        print("{} Start training...".format(datetime.now()))
-        print("{} Open Tensorboard: tensorboard --logdir {}".format(datetime.now(), tensorboard_path + '/training'))
-        print("{} Open http://0.0.0.0:6006/ into your web browser".format(datetime.now()))
-
-        train_feed_dict = {x: inputs, y: output}
-        test_feed_dict = {x: inputs_test, y: output_test}
         for step in range(TRAIN_STEPS):
-            session.run(train, feed_dict=train_feed_dict)
+            # Create a partition of the training dataset
+            train_partition, validation_partition = train_test_split(train_df, test_size=0.2)
+
+            # Train with the training partition
+            session.run(train, feed_dict={
+                x: train_partition[input_cols].values,
+                y: train_partition[[output_col]].values
+            })
+
+            # When logging, evaluate also with the validation partition and the test
             if TRAIN_STEPS % LOGS_STEPS == 0:
-                summary = session.run(merged_summary, feed_dict=train_feed_dict)
+                summary = session.run(merged_summary, feed_dict={
+                    x: train_partition[input_cols].values,
+                    y: train_partition[[output_col]].values
+                })
                 writer_trn.add_summary(summary, step)
                 writer_trn.flush()
-                summary = session.run(merged_summary, feed_dict=test_feed_dict)
+                summary = session.run(merged_summary, feed_dict={
+                    x: validation_partition[input_cols].values,
+                    y: validation_partition[[output_col]].values
+                })
+                writer_val.add_summary(summary, step)
+                writer_val.flush()
+                summary = session.run(merged_summary, feed_dict={
+                    x: test_df[input_cols].values,
+                    y: test_df[[output_col]].values
+                })
                 writer_tst.add_summary(summary, step)
                 writer_tst.flush()
+
+        # Write results to a file so we can later make graphs
+        pd.DataFrame({
+            'expected': test_df[[output_col]].values,
+            'real': session.run(y_hat, feed_dict={
+                x: test_df[input_cols].values,
+                y: test_df[[output_col]].values
+            }),
+        }).to_csv('fcs-{}-{}.csv'.format(SUBJECT, num_fs_string), index=None)
+
+    tb_process.join()
